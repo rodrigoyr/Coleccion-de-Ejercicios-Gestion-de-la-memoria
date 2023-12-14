9ce7df17cf60c135838c935d97874cce2d39db4d
rodrigoyr <ryeperub@myuax.com>
2023-12-14 11:00:10 +0100

    code commit commit

--- Ejercicio/main.cpp
+++ Ejercicio/main.cpp
@@ -1,6 +1,33 @@
-#include <iostream>
+#include <sys/mman.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+#define SIZE 4096
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    char *shared_memory = mmap(NULL, SIZE, PROT_READ|PROT_WRITE, MAP_SHARED|MAP_ANONYMOUS, -1, 0);
+    if (shared_memory == MAP_FAILED) {
+        perror("mmap");
+        exit(EXIT_FAILURE);
+    }
+
+    pid_t pid = fork();
+
+    if (pid < 0) {
+        perror("fork");
+        exit(EXIT_FAILURE);
+    } else if (pid == 0) {
+        printf("Child reads: %s\n", shared_memory);
+        munmap(shared_memory, SIZE);
+        exit(EXIT_SUCCESS);
+    } else {
+        strcpy(shared_memory, "Hello, child process!");
+        wait(NULL);
+        munmap(shared_memory, SIZE);
+    }
+
     return 0;
 }
